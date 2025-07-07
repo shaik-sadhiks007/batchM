3d105f998942b58fce155c03e8565bc02687db17
shaik-sadhiks007 <shaik.sadhiks007@gmail.com>
2025-07-07 19:14:04 +0530

    class

---
+++ js/advancedjs/class.js
@@ -0,0 +1,92 @@
+
+
+// class ---- blueprint (example : blueprint of the house) --- what to do 
+
+// while creating the class , name should be capital
+// class Car {
+
+
+
+// }
+
+
+// functions --- 1000
+
+
+// collections of functions --- class --- math, person, students
+
+
+// collections of class ---- package 
+
+
+class Car {
+
+    constructor(name,year) {
+
+        this.carname = name
+
+        this.year = year
+
+    }
+
+    // function in the class known as method
+
+    setthevariables(name, year) {
+
+        this.carname = name
+
+        this.year = year
+
+    }
+
+
+    info() {
+
+        console.log(this.carname, 'name of the car')
+
+        console.log(this.year, 'year')
+
+    }
+
+    drive() {
+        console.log(this.carname, 'drive')
+    }
+
+
+}
+
+// object creation of the Class
+
+// object ---- function call
+
+// object is a instance of the class,
+
+
+let myCar = new Car('tesla',2020)
+
+// myCar.setthevariables('tesla', 2020)
+
+// myCar.info()
+
+
+// myCar.setthevariables('bmw', 2022)
+
+myCar.info()
+
+
+
+
+// myCar.drive()
+
+console.log(myCar.carname, 'name') // accessing the variables
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/advancedjs/const.js
@@ -0,0 +1,25 @@
+
+const name = 'sadhik'
+
+// we are assigning, 
+
+// name = 'hemanth'
+
+console.log(name,'name')
+
+const obj = {
+    username : 'sadhik',
+    age : 70
+}
+
+
+// in the const --- u can't assign new variable but u can modify properties
+// obj = {
+//     username : 'bhanu',
+//     age : 80
+// }
+
+
+// obj.username = 'hemanth'
+
+console.log(obj,'obj')
